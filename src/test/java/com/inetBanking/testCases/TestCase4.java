0dc2dea21d050475b180e5d42a0fdd2038e43139
Satya Dvarakonda <Satya Dvarakonda@dell-personal1725.home>
2024-02-03 11:03:57 -0600

    comitingtest2and3intoBranchM

---
+++ src/test/java/com/inetBanking/testCases/TestCase4.java
@@ -0,0 +1,42 @@
+package com.inetBanking.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetBanking.pageObjects.LoginPage;
+
+public class TestCase4 extends BaseClass
+{
+	
+	@Test
+	public void loginTest()
+	{
+		driver.get(baseURL);
+		Logger.info("url is opened");
+		LoginPage lp = new LoginPage(driver);
+		Logger.info("username input");
+		
+		lp.setUserName(username);
+		Logger.info("password input");
+		lp.setPassword(password);
+		Logger.debug("submit click");
+		lp.clickSubmit();
+		
+		  String j = driver.getTitle();
+		  System.out.println("Your page title Is : "+j);
+		  Logger.debug("check for title with assertion");
+		  Assert.assertEquals("Guru99 Bank Manager HomePage", driver.getTitle());
+
+	//if(driver.getTitle().contains(" Hotels by Hilton - Book the Best Rates Across All Brands "))
+		//if(driver.getTitle().contains(" Guru99 Bank Home Page "))
+		/*{
+			Assert.assertTrue(true);
+			
+		}
+		else {
+			Assert.assertTrue(false);
+		}
+	}*/
+
+}
+}
